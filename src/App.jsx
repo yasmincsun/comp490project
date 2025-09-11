bd314621b65b40849c8591b094a6fbadb2f1204b
yasmincsun <yasmin.zubair.180@my.csun.edu>
2025-09-11 01:08:11 +0000

    changes to url and UI appearance

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+// import musicNotes from 'public/musical-notes.svg'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,14 +10,9 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Access the Music
       </div>
-      <h1>Vite + React</h1>
+      <h1>Music App</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
