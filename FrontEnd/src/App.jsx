dcad208bae122c4555ebdd96e48e2835603d65ef
yasmincsun <yasmin.zubair.180@my.csun.edu>
2025-09-29 23:32:49 +0000

    frontend changes

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { QuizProvider } from "./context/quiz-context";
 import Quiz from "./components/quiz";
 import Login from "./components/loginPage/login"
+import Home from "./components/homePage/homePage"
+// import Main from 
 function App() {
   return (
 
     
-        <Login />
+        <Home />
 
 
 
