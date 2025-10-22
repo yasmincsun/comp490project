cdc1301ed37d99f85ffc4490f0bce05b5768881a
yasmincsun <yasmin.zubair.180@my.csun.edu>
2025-10-22 16:45:38 -0700

    deleted quiz template

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,24 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QuizProvider } from "./context/quiz-context";
-import Quiz from "./components/quiz";
 import ProfilePage from "./components/profilePage/profilePage";
 import Login from "./components/loginPage/login";
 import HomePage from "./components/homePage/homePage";
 
 function App() {
   return (
-    <QuizProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/quiz" element={<Quiz />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/profile" element={<ProfilePage />} />
         </Routes>
       </BrowserRouter>
-    </QuizProvider>
   );
 }
 
