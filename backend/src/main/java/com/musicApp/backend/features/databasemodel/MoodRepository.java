f82715d299e68cf36129f6ef43398c847874b7b1
Miguel Alfaro <miguel.alfaroconcha.213@my.csun.edu>
2025-10-22 17:02:36 -0700

    Adding JPA files from backend branch

---
+++ backend/src/main/java/com/musicApp/backend/features/databasemodel/MoodRepository.java
@@ -0,0 +1,9 @@
+package com.musicApp.backend.features.databasemodel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MoodRepository extends JpaRepository<Mood, Integer> {
+   
+}
