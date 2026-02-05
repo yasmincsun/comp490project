436dc076de37c43cbad3595cfefaacb4f9e115c9
Miguel Alfaro <miguel.alfaroconcha.213@my.csun.edu>
2026-02-04 17:12:37 -0800

    Unfinished post.jsx/.css

--- FrontEnd/src/components/postPage/post.jsx
+++ FrontEnd/src/components/postPage/post.jsx
@@ -1,3 +1,155 @@
 //Refer to Django + React video
 //It's written in Python, make Java equivalent for backend
 //Ammend it so it doesn't require an image upload, and so the category is written instead of a drop-down menu
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { useForm } from "react-hook-form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectGroup,
+  SelectLabel,
+} from "@/components/ui/select";
+import InputError from "@/ui_components/InputError";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createBlog } from "@/services/apiBlog";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import SmallSpinner from "@/ui_components/SmallSpinner";
+import SmallSpinnerText from "@/ui_components/SmallSpinnerText";
+
+const CreatePostPage = () => {
+  const { register, handleSubmit, formState, setValue } = useForm();
+  const { errors } = formState;
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (data) => createBlog(data),
+    onSuccess: () => {
+      toast.success("New post added successfully");
+      queryClient.invalidateQueries({ queryKey: ["blogs"] });
+      navigate("/");
+    },
+  });
+
+  function onSubmit(data) {
+    const formData = new FormData();
+    formData.append("title", data.title);
+    formData.append("content", data.content);
+    formData.append("category", data.category);
+    if (data.featured_image) {
+      formData.append("featured_image", data.featured_image[0]);
+    }
+
+    mutation.mutate(formData);
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="md:px-16 px-8 py-6 flex flex-col mx-auto my-9 items-center gap-6 w-fit rounded-lg bg-[#FFFFFF] shadow-xl dark:text-white dark:bg-[#141624]"
+    >
+      <div className="flex flex-col gap-2 justify-center items-center mb-2">
+        <h3 className="font-semibold text-2xl max-sm:text-xl">Create Post</h3>
+
+        <p className="max-sm:text-[14px]">
+          Create a new post and share your ideas.
+        </p>
+      </div>
+
+      <div>
+        <Label htmlFor="title" className="dark:text-[97989F]">
+          Title
+        </Label>
+        <Input
+          type="text"
+          id="title"
+          {...register("title", {
+            required: "Title is required",
+            minLength: {
+              value: 3,
+              message: "The title must be at least 3 characters",
+            },
+          })}
+          placeholder="Give your post a title"
+          className="border-2 border-[#141624] dark:border-[#3B3C4A] focus:outline-0 h-[40px] w-[400px] max-sm:w-[300px] max-sm:text-[14px]"
+        />
+
+        {errors?.title?.message && <InputError error={errors.title.message} />}
+      </div>
+
+    
+      <div>
+        <Label htmlFor="content">Content</Label>
+        <Textarea
+          id="content"
+          placeholder="Write your caption"
+          {...register("content", {
+            required: "Content is required",
+            minLength: {
+              value: 10,
+              message: "The content must be at least 10 characters",
+            },
+          })}
+          className="border-2 border-[#141624] dark:border-[#3B3C4A] focus:outline-0 h-[180px]  w-[400px] text-justify max-sm:w-[300px] max-sm:text-[14px]"
+        />
+        {errors?.content?.message && (
+          <InputError error={errors.content.message} />
+        )}
+      </div>
+
+        //Asks user to type in a category for their post
+        
+      <div className="w-full">
+        <Label htmlFor="category">Category</Label>
+        <Textarea
+        id ="content"
+        placeholder= "Add your category"
+        {...register("content", {
+            required: "Post category is required"
+
+        })}
+        className="border-2 border-[#141624] dark:border-[#3B3C4A] focus:outline-0 h-[180px]  w-[400px] text-justify max-sm:w-[300px] max-sm:text-[14px]"
+        />
+        </div>
+
+       
+      <div className="w-full">
+        <Label htmlFor="featured_image">Featured Image</Label>
+        <Input
+          type="file"
+          id="picture"
+          {...register("featured_image", {
+            required: "Blog's featured image is required",
+          })}
+          className="border-2 border-[#141624] dark:border-[#3B3C4A] focus:outline-0 h-[40px] w-full max-sm:w-[300px] max-sm:text-[14px]"
+        />
+
+        {errors?.featured_image?.message && (
+          <InputError error={errors.featured_image.message} />
+        )}
+      </div>
+
+      <div className="w-full flex items-center justify-center flex-col my-4">
+        <button disabled={mutation.isPending} className="bg-[#4B6BFB] text-white w-full py-3 px-2 rounded-md flex items-center justify-center gap-2">
+          {mutation.isPending ? (
+            <>
+              {" "}
+              <SmallSpinner /> <SmallSpinnerText text="Creating post..." />{" "}
+            </>
+          ) : (
+            <SmallSpinnerText text="Create post" />
+          )}
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default CreatePostPage;
