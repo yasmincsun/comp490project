5276bf1d26e963d68f8a0690e0f90870326f5b51
yasmincsun <yasmin.zubair.180@my.csun.edu>
2026-03-30 17:44:58 -0700

    Revert "Merge branch 'main' of https://github.com/yasmincsun/comp490project"
    This reverts commit 04c3ab4794ba6345617a94a5438829c029c705f1, reversing
    changes made to b304981bbbef2b82c90b63d0a7c9d605ece93622.

---
+++ music/app/test/java/com/moodymusic/music/MusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moodymusic.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
